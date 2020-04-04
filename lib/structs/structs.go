b6ef9064db2a44e936e8c63db58cd762e520a32f
Iordanis Paschalidis <iordanis@broadbean.com>
2020-04-04 20:56:35 +0100

    adding more docs to lib/structs/structs.go

--- lib/structs/structs.go
+++ lib/structs/structs.go
@@ -1,9 +1,16 @@
 package structs
 
+/*
+	structs that are used through the service
+*/
+
+// Countries is being used in controller/sort/sort.go,
+// lib/caching/caching.go and lib/stats/stats.go
 type Countries struct {
 	Data []Country `json:"data"`
 }
 
+// Country is being use in lib/curve/curve.go and lib/stats/stats.go
 type Country struct {
 	Country            string  `json:"country"`
 	Cases              int     `json:"cases"`
@@ -16,21 +23,25 @@ type Country struct {
 	CasesPerOneMillion float64 `json:"casesPerOneMillion"`
 }
 
+// Compare is being used in lib/curve/curve.go
 type Compare struct {
 	CountryOne CompareData `json:"countryOne"`
 	CountryTwo CompareData `json:"countryTwo"`
 }
 
+// CompareData is being used in lib/curve/curve.go
 type CompareData struct {
 	Country string    `json:"country"`
 	Data    []float64 `json:"data"`
 }
 
+// CountryCurve is being used in lib/curve/curve.go
 type CountryCurve struct {
 	Country  string         `json:"country"`
 	Timeline TimelineStruct `json:"timeline"`
 }
 
+// TimelineStruct is being used in lib/curve/curve.go
 type TimelineStruct struct {
 	Cases     interface{} `json:"cases"`
 	Deaths    interface{} `json:"deaths"`
