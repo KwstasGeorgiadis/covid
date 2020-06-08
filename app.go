5f52f75d0b845a816a3ddfc86d37d1d68edb4786
Iordanis Paschalidis <iordanis@ipaschalidis.com>
2020-06-08 15:35:44 +0100

    refactoring

--- app.go
+++ app.go
@@ -509,7 +509,7 @@ func compareCasesHandle(w http.ResponseWriter, r *http.Request) {
 }
 
 /*
-	POST request to /compare/cases/unique endpoint
+	POST request to /api/compare/cases/unique endpoint
 
 	Request:
 
@@ -561,7 +561,131 @@ func compareUniqueCasesHandle(w http.ResponseWriter, r *http.Request) {
 		"Endpoint /compare/cases/unique called with response JSON body "+string(jsonBody), status, elapsed)
 }
 
-//----------------------------------------------------------------------------------------
+/*
+	POST request to /api/compare/all endpoint
+
+	Request:
+
+	{
+		"countryOne" : "Spain",
+		"countryTwo" : "Italy"
+	}
+
+	Response
+
+	{
+    "countryOne": {
+        "country": "Spain",
+        "dataDeaths": [
+            28,
+            27888,
+            27940,
+            28628,
+            28678,
+            28752
+        ],
+        "dataDeathsFromFirst": [
+            28,
+            27888,
+            27940,
+            28628,
+            28678,
+            28752
+        ],
+        "dataDeathsPerDay": [
+            110,
+            52,
+            688,
+            50,
+            74
+        ],
+        "dataRecoverd": [
+            150376,
+            150376,
+            150376,
+            150376,
+            150376
+        ],
+        "dataCases": [
+            15,
+            32,
+            45,
+            84,
+            120,
+            165,
+            222,
+            259,
+        ],
+        "dataCasesFromFirst": [
+            159,
+            294,
+            394,
+            334,
+            318,
+            332,
+            240
+        ]
+    },
+    "countryTwo": {
+        "country": "Spain",
+        "dataDeaths": [
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+        "dataDeathsFromFirst": [
+            33530,
+            33601,
+            33689,
+            33774,
+            33846,
+            33899
+        ],
+        "dataDeathsPerDay": [
+            1,
+            1,
+            1,
+            4,
+            3,
+            2,
+        ],
+        "dataRecoverd": [
+            0,
+            1,
+            1,
+            1,
+            2,
+            3,
+            45,
+            46,
+            46,
+        ],
+        "dataCases": [
+            232664,
+            232997,
+            233197,
+            233515,
+            233836,
+            234013,
+            234531,
+            234801,
+            234998
+        ],
+        "dataCasesFromFirst": [
+            200,
+            318,
+            321,
+            177,
+            518,
+            270,
+            197
+        ]
+    }
+}
+}
+*/
 func compareAllHandle(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -575,7 +699,7 @@ func compareAllHandle(w http.ResponseWriter, r *http.Request) {
 }
 
 /*
-	Get request to /news with no parameters
+	Get request to /api/news with no parameters
 
 	Response:
 
@@ -609,11 +733,11 @@ func newsHandle(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBody)
 	elapsed := time.Since(start).Seconds()
 	applogger.LogHTTP("INFO", "main", "newsHandle",
-		"Endpoint /compare/percent called with response JSON body "+string(jsonBody), status, elapsed)
+		"Endpoint /api/news called with response JSON body "+string(jsonBody), status, elapsed)
 }
 
 /*
-	Get request to /news/vaccine with no parameters
+	Get request to /api/news/vaccine with no parameters
 
 	Response:
 
@@ -647,11 +771,11 @@ func newsVaccineHandle(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBody)
 	elapsed := time.Since(start).Seconds()
 	applogger.LogHTTP("INFO", "main", "newsVaccineHandle",
-		"Endpoint /compare/percent called with response JSON body "+string(jsonBody), status, elapsed)
+		"Endpoint /api/news/vaccine called with response JSON body "+string(jsonBody), status, elapsed)
 }
 
 /*
-	Get request to /news/treatment with no parameters
+	Get request to /api/news/treatment with no parameters
 
 	Response:
 
@@ -685,9 +809,83 @@ func newsTreatmentHandle(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBody)
 	elapsed := time.Since(start).Seconds()
 	applogger.LogHTTP("INFO", "main", "newsTreatmentHandle",
-		"Endpoint /compare/percent called with response JSON body "+string(jsonBody), status, elapsed)
+		"Endpoint /api/news/treatment called with response JSON body "+string(jsonBody), status, elapsed)
 }
 
+/*
+	Get request to /api/news/all with no parameters
+
+	Response:
+
+	{
+    "vaccine": {
+        "data": [
+            {
+                "title": "Here's where we stand on getting a coronavirus vaccine - CNN",
+                "description": "<ol><li><a href=\"https://www.cnn.com/2020/06/08/health/covid-19-vaccine-latest/index.html\" target=\"_blank\">Here's where we stand on getting a coronavirus vaccine</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">CNN</font></li><li><a href=\"https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(20)31252-6/fulltext\" target=\"_blank\">COVID-19 vaccine development pipeline gears up</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">The Lancet</font></li><li><a href=\"https://www.healthline.com/health-news/why-companies-are-making-billions-of-covid-19-vaccine-doses-that-may-not-work\" target=\"_blank\">Why Companies Are Making Billions of COVID-19 Vaccine Doses</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">Healthline</font></li><li><a href=\"https://www.weforum.org/agenda/2020/06/astrazeneca-covid19-vaccine-gates-foundation\" target=\"_blank\">Pharmaceutical company pledges 2 billion COVID-19 vaccine doses</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">World Economic Forum</font></li><li><a href=\"https://www.usnews.com/news/health-news/articles/2020-06-08/experts-optimistic-in-search-for-covid-19-vaccine\" target=\"_blank\">Experts Optimistic in Search for COVID-19 Vaccine | Health News</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">U.S. News & World Report</font></li><li><strong><a href=\"https://news.google.com/stories/CAAqOQgKIjNDQklTSURvSmMzUnZjbmt0TXpZd1NoTUtFUWpyNnZMMmo0QU1FWTRkbWhyVmgyeWxLQUFQAQ?oc=5\" target=\"_blank\">View Full Coverage on Google News</a></strong></li></ol>",
+                "url": "https://www.cnn.com/2020/06/08/health/covid-19-vaccine-latest/index.html",
+                "urlToImage": "",
+                "publishedAt": "Mon, 08 Jun 2020 12:12:50 GMT",
+                "source": "CNN",
+                "sourceURL": "https://www.cnn.com"
+            },
+            {
+                "title": "COVID-19 vaccine trials bring hope for many but come too late for this family - NBC News",
+                "description": "<a href=\"https://www.nbcnews.com/health/health-news/covid-19-vaccine-trials-bring-hope-many-come-too-late-n1226716\" target=\"_blank\">COVID-19 vaccine trials bring hope for many but come too late for this family</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">NBC News</font>",
+                "url": "https://www.nbcnews.com/health/health-news/covid-19-vaccine-trials-bring-hope-many-come-too-late-n1226716",
+                "urlToImage": "",
+                "publishedAt": "Sun, 07 Jun 2020 16:02:58 GMT",
+                "source": "NBC News",
+                "sourceURL": "https://www.nbcnews.com"
+            }
+        ]
+    },
+    "treament": {
+        "data": [
+            {
+                "title": "U.S. government’s supply of COVID-19 treatment drug, remdesivir, will run out at the end of the month - KTLA",
+                "description": "<a href=\"https://ktla.com/news/coronavirus/u-s-governments-supply-of-covid-19-treatment-drug-remdesivir-will-run-out-at-the-end-of-the-month/\" target=\"_blank\">U.S. government’s supply of COVID-19 treatment drug, remdesivir, will run out at the end of the month</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">KTLA</font>",
+                "url": "https://ktla.com/news/coronavirus/u-s-governments-supply-of-covid-19-treatment-drug-remdesivir-will-run-out-at-the-end-of-the-month/",
+                "urlToImage": "",
+                "publishedAt": "Mon, 08 Jun 2020 02:39:00 GMT",
+                "source": "KTLA",
+                "sourceURL": "https://ktla.com"
+            },
+            {
+                "title": "Drug Trial Planned For Synthetic Cannabinoid COVID-19 Treatment - The Fresh Toast",
+                "description": "<a href=\"https://thefreshtoast.com/cannabis/drug-trial-planned-for-synthetic-cannabinoid-covid-19-treatment/\" target=\"_blank\">Drug Trial Planned For Synthetic Cannabinoid COVID-19 Treatment</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">The Fresh Toast</font>",
+                "url": "https://thefreshtoast.com/cannabis/drug-trial-planned-for-synthetic-cannabinoid-covid-19-treatment/",
+                "urlToImage": "",
+                "publishedAt": "Mon, 08 Jun 2020 13:36:15 GMT",
+                "source": "The Fresh Toast",
+                "sourceURL": "https://thefreshtoast.com"
+            }
+        ]
+    },
+    "news": {
+        "data": [
+            {
+                "title": "What you need to know about the COVID-19 pandemic on 8 June - World Economic Forum",
+                "description": "<a href=\"https://www.weforum.org/agenda/2020/06/covid-19-what-you-need-to-know-about-the-coronavirus-pandemic-on-8-june/\" target=\"_blank\">What you need to know about the COVID-19 pandemic on 8 June</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">World Economic Forum</font>",
+                "url": "https://www.weforum.org/agenda/2020/06/covid-19-what-you-need-to-know-about-the-coronavirus-pandemic-on-8-june/",
+                "urlToImage": "",
+                "publishedAt": "Mon, 08 Jun 2020 08:51:16 GMT",
+                "source": "World Economic Forum",
+                "sourceURL": "https://www.weforum.org"
+            },
+            {
+                "title": "How Reskilling Can Soften the Economic Blow of Covid-19 - Harvard Business Review",
+                "description": "<a href=\"https://hbr.org/2020/06/how-reskilling-can-soften-the-economic-blow-of-covid-19\" target=\"_blank\">How Reskilling Can Soften the Economic Blow of Covid-19</a>&nbsp;&nbsp;<font color=\"#6f6f6f\">Harvard Business Review</font>",
+                "url": "https://hbr.org/2020/06/how-reskilling-can-soften-the-economic-blow-of-covid-19",
+                "urlToImage": "",
+                "publishedAt": "Mon, 08 Jun 2020 12:10:46 GMT",
+                "source": "Harvard Business Review",
+                "sourceURL": "https://hbr.org"
+			}
+        ]
+    }
+}
+*/
 func newsAllHandle(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -697,9 +895,65 @@ func newsAllHandle(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBody)
 	elapsed := time.Since(start).Seconds()
 	applogger.LogHTTP("INFO", "main", "newsAllHandle",
-		"Endpoint /compare/percent called with response JSON body "+string(jsonBody), status, elapsed)
+		"Endpoint /api/news/all called with response JSON body "+string(jsonBody), status, elapsed)
 }
 
+/*
+	Get request to /api/world with no parameters
+
+	Response:
+
+{
+    "cases": [
+        555,
+        654,
+        941,
+        1434,
+
+    ],
+    "deaths": [
+        17,
+        18,
+        26,
+        42,
+        56,
+        82,
+    ],
+    "recovered": [
+        28,
+        30,
+        36,
+        39,
+        52,
+    ],
+    "casesDaily": [
+        99,
+        287,
+        493,
+        684,
+        809,
+    ],
+    "deathsDaily": [
+        1,
+        8,
+        16,
+        14,
+        26,
+        49,
+    ],
+    "recoveredDaily": [
+        2,
+        6,
+        3,
+        13,
+        9,
+        46,
+        19,
+        17,
+    ]
+}
+}
+*/
 func worldHandle(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -709,9 +963,113 @@ func worldHandle(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBody)
 	elapsed := time.Since(start).Seconds()
 	applogger.LogHTTP("INFO", "main", "worldHandle",
-		"Endpoint /compare/percent called with response JSON body "+string(jsonBody), status, elapsed)
+		"Endpoint /api/world called with response JSON body "+string(jsonBody), status, elapsed)
 }
 
+/*
+	Get request to /api/hotspot with no parameters
+
+	Response:
+
+{
+    "mostCases": {
+        "country": "Brazil",
+        "data": [
+            20599,
+            26417,
+            26928,
+            33274,
+            16409,
+            11598,
+            28936,
+            28633,
+            30925,
+            30830,
+            27075
+        ]
+    },
+    "secondCases": {
+        "country": "USA",
+        "data": [
+            18263,
+            22577,
+            24266,
+            24146,
+            20007,
+            20848,
+            20801,
+            19699,
+            21140,
+            24720,
+            22681
+        ]
+    },
+    "thirdCases": {
+        "country": "Russia",
+        "data": [
+            8338,
+            8371,
+            8572,
+            8952,
+            9268,
+            8485,
+            8858,
+            8529,
+            8823,
+            8718,
+            8846
+        ]
+    },
+    "mostDeaths": {
+        "country": "USA",
+        "data": [
+            1505,
+            1199,
+            1193,
+            967,
+            605,
+            768,
+            1031,
+            995,
+            1036,
+            921,
+            670
+        ]
+    },
+    "secondDeaths": {
+        "country": "Brazil",
+        "data": [
+            1086,
+            1156,
+            1124,
+            956,
+            480,
+            623,
+            1262,
+            1349,
+            1473,
+            1005,
+            904
+        ]
+    },
+    "thirdDeaths": {
+        "country": "Mexico",
+        "data": [
+            463,
+            447,
+            371,
+            364,
+            151,
+            237,
+            470,
+            1092,
+            816,
+            625,
+            341
+        ]
+    }
+}
+*/
 func hotspotHandle(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -720,15 +1078,15 @@ func hotspotHandle(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(status)
 	w.Write(jsonBody)
 	elapsed := time.Since(start).Seconds()
-	applogger.LogHTTP("INFO", "main", "worldHandle",
-		"Endpoint /compare/percent called with response JSON body "+string(jsonBody), status, elapsed)
+	applogger.LogHTTP("INFO", "main", "hotspotHandle",
+		"Endpoint /api/hotspot called with response JSON body "+string(jsonBody), status, elapsed)
 }
 
 /*
 	Running the server in port 9080 (getting the value from ./config/covid.json )
 
 	"server" : {
-                "port" : ":9080"
+        "port" : ":9080"
     },
 
 	Endpoints:
